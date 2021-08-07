d891f6b0f2a5067f8dab9756edca22e02080b6bf
Jeff Oliver <jcoliver@email.arizona.edu>
2021-08-06 18:26:43 -0700

    Add draft of mapping in report

--- R/MapSites.R
+++ R/MapSites.R
@@ -1,8 +1,15 @@
 #' Create map of sites identified with highest number of missing species
 #' 
-#' 
+#' @param sites data frame with at least the following columns:
+#' \describe{
+#'   \item{locName}{character name of site}
+#'   \item{num_new}{integer number of new species at site}
+#'   \item{lat}{double latitude in decimal degrees}
+#'   \item{lng}{double longitude in decimal degrees}
+#' }
+#'  
+#' @import ggplot2
 #' @importFrom ggmap get_map ggmap
-#' @export
 MapSites <- function(sites) {
 
   # values coming in from data frame 'sites':
@@ -17,49 +24,48 @@ MapSites <- function(sites) {
 
   # Determine bounds of map; want to think about centers where international 
   # date line might get involved...
+  # Only need to worry about this when longitudes are really around (and on 
+  # both sides) if International Date Line
+  # Buuuuut, stamen maps (and maybe ggmap) can't handle this right now.
+  # if (any(sites$lng > 90) & any(sites$lng < -90)) {
+  #   # First subtract 360 from any positive longitudes, to make them more 
+  #   # negative
+  #   sites$lng[sites$lng > 0] <- sites$lng[sites$lng > 0] - 360
+  #   # Now find westernmost longitude
+  #   left <- min(sites$lng) + 360
+  #   right <- max(sites$lng)
+  #   sites$lng[sites$lng < -180] <- sites$lng[sites$lng < -180] + 360
+  # } else {
+  #   # Normal math works, no temporary transformation necessary
+  #   left <- min(sites$lng)
+  #   right <- max(sites$lng)
+  # }
   
+  # Round up/down as appropriate to reduce the chance that points lie on map
+  # edges
+  left <- floor(min(sites$lng))
+  right <- ceiling(max(sites$lng))
+  top <- ceiling(max(sites$lat))
+  bottom <- floor(min(sites$lat))
 
+  map_bounds <- c(left, bottom, right, top)
   
-  # From Target-report.Rmd below here
-  map.data <- lapply(target.list, "[", c("site.name", "num.species", "coordinates"))
-  map.data <- map.data[1:num.to.print]
-  map.df <- data.frame(site.name = as.character(names(map.data)),
-                       num.species = as.vector(unlist(sapply(map.data, "[", "num.species"))),
-                       latitude = t(sapply(map.data, "[[", "coordinates"))[, "lat"],
-                       longitude = t(sapply(map.data, "[[", "coordinates"))[, "lng"])
-  # Oddly, this does *not* work if we try to do it when initializing df...
-  map.df$site.name <- as.character(map.df$site.name)
-  
-  # Order by decreasing # species (could have been done already)
-  map.df <- map.df[order(map.df$num.species, decreasing = TRUE), ]
-  rownames(map.df) <- NULL
-  
-  # Make a column with abbreviated name and unseen species count
-  map.df$print.name <- paste0(rownames(map.df), " - ", 
-                              substr(x = map.df$site.name,
-                                     start = 1,
-                                     stop =  10))
-  map.df$print.name <- paste0(map.df$print.name, " (", map.df$num.species, ")")
-  
-  map.bounds <- c(floor(min(c(map.df$longitude, center.lng))),
-                  floor(min(c(map.df$latitude, center.lat))),
-                  ceiling(max(c(map.df$longitude, center.lng))),
-                  ceiling(max(c(map.df$latitude, center.lat))))
-  
-  # TODO: Move mapping to functions file and place inside a conditional in case
-  # retrival via ggmap::get_map times out
-  center.map <- get_map(location = map.bounds, 
-                        source = "stamen", 
-                        maptype = "terrain")
-  
-  targets.map <- ggmap(center.map) +
-    geom_point(data = map.df,
-               mapping = aes(x = longitude, y = latitude, color = print.name),
+  # TODO: Need catch to deal with time out of ggmap::get_map
+  center_map <- ggmap::get_map(location = map_bounds, 
+                               source = "stamen", 
+                               maptype = "terrain")
+
+  # TODO: Need to color by site name (locName), but use print_name for legend
+  # Edge case is Mt. Lemmon sites, which have unique info cutoff when substring
+  # only pulls out 12 characters
+  sites_map <- ggmap::ggmap(ggmap = center_map) +
+    geom_point(data = sites,
+               mapping = aes(x = lng, y = lat, color = print_name),
                size = 3) +
     scale_color_brewer(name = "Site", palette = "Dark2") +
-    theme_bw() +
+    theme_minimal() +
     xlab(label = "Longitude") +
     ylab(label = "Latitude")
-  
-  print(targets.map)  
+
+  return(sites_map)
 }
